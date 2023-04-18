6cd1cd4e3da2430d2fc13803c745dd862b12c6b8
jackaverill91 <119092671+jackaverill91@users.noreply.github.com>
2023-04-18 21:03:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ import presentDrinks from "./src/presentDrinks.js";
 const URL = 'www.thecocktaildb.com/api/json/v1/1/search.php?s=a';
 
 window.addEventListener('DOMContentLoaded', () => {
-  presentDrinks(URL); // arrow functionality changes this back to a callback so URL can be passed as param
+  presentDrinks(URL);
 });
