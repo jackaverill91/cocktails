0041a1f4a4ff3c6cd7ccf99fd35c2bacf198b2ed
jackaverill91 <119092671+jackaverill91@users.noreply.github.com>
2023-04-18 21:02:37 +0700

    Add files via upload

---
+++ src/presentDrinks.js
@@ -0,0 +1,10 @@
+import fetchDrinks from "./fetchDrinks.js";
+
+const showDrinks = async (url) => {
+  const data = await fetchDrinks(url);
+  console.log(data);
+  // fetch drinks
+  // display drinks
+}
+
+export default showDrinks;
